920fcd42f06011414cb3d1515b34799b2629b535
Mohamed_euler <96792977+mohamed-euler@users.noreply.github.com>
2023-03-08 03:28:25 +0200

    Update readData.py
    update reading file

--- readData.py
+++ readData.py
@@ -1,34 +1,47 @@
 import numpy as np
-from PIL import Image
-import os
+# take a path of pgm image and return vector represent the image data
+def read_image(image_path):
+    f = open(image_path, 'rb')
+    # Skip the first 3 lines
+    f.readline()
+    f.readline()
+    f.readline()
+    raster = []
+    # read the data
+    for h in range(112 * 92):
+        row = [ord(f.read(1))]
+        raster.append(row)
+    return np.array(raster).T
 
-def read_images():
-    data = []
-    labels = []
-    for i in range(1, 41):
-        for j in range(1, 11):
-            # path where data stored , you must change it according to your computer
-            img_path = r"/content/drive/MyDrive/archive/s" + str(i) + "/" + str(j) + ".pgm"
-            img = Image.open(img_path)
-            img_vector = np.array(img).flatten()
-            data.append(img_vector)
-            labels.append(i)
-    return np.array(data), np.array(labels)
+
+# this function read all image files and return matrix of images data and label vector
+def read_data():
+    d = np.zeros((400, 112 * 92))
+    y = np.zeros(400)
+    for i in range(40):
+        for j in range(10):
+            path = r"C:\Users\lenovo\Desktop\Term8\pattern\labs\lab1\archive\s" + str(i + 1) + "\\" + str(
+                j + 1) + ".pgm"
+
+            d[i * 10 + j, :] = np.array(read_image(path))
+            # Add label to label vector
+            y[i * 10 + j] = i + 1
+    return np.array(d), np.array(y)
+
 
 # function get data matrix and label vector and split each one to two matrix , one as a samples and one for testing
 def data_Split(data, y):
-    d_test = data[1::2 , :]     # odd data
-    d_samples = data[::2 , :]   # even data
-    l_samples = y[::2]          # label for even data
-    l_test = y[1::2]            # label for odd data
+    y_samples = []
+    y_test = []
+    d_samples = []
+    d_test = []
+    i = 0
+    while i < len(y) - 1:
+        y_samples.append(y[i])
+        y_test.append(y[i + 1])
+        d_samples.append(data[i])
+        d_test.append(data[i + 1])
+        i += 2
 
-    return d_samples, d_test, l_samples, l_test 
+    return np.array(d_samples), np.array(d_test), np.array(y_samples), np.array(y_test)
 
-data ,y= read_images()
-d_samples, d_test, y_samples, y_test = data_Split(data,y)
-
-
-# print(d_samples)
-# print(d_test)
-# print(y_samples)
-# print(y_test)
